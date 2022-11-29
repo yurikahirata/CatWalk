8159f54842706748d59c2a1516a1a43aabb273a5
hippothebrave <adigal03@gmail.com>
2022-11-29 12:47:58 +0100

    updated script

--- Assets/Scripts/testCharacterController.cs
+++ Assets/Scripts/testCharacterController.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class testCharacterController : MonoBehaviour
 {
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
 
-    private CharacterController characterController;
-    public float speed;
+    private Vector3 moveDirection = Vector3.zero;
 
+    CharacterController controller;
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        characterController.Move(move * Time.deltaTime * speed);
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
