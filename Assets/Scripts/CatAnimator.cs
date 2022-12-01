bdb5caf13b98c29ddba867963ced4c761b1a2c29
hippothebrave <adigal03@gmail.com>
2022-12-01 18:06:34 +0100

    Cat Animation + Movement

---
+++ Assets/Scripts/CatAnimator.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAnimator : MonoBehaviour
+{
+    protected Animator animator;
+    public float timer;
+    public float waitToTurnHead = 5;
+    private CharacterController cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        timer = 0f;
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // walk when moving
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            Debug.Log("Moving.");
+            animator.SetInteger("motion", 1);
+            animator.SetBool("turnidle", false);
+        }
+
+        //stop moving when there is no input
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            Debug.Log("Stopping.");
+            animator.SetInteger("motion", 0);
+        }
+
+        //check current state
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        //if current state is Idle_A, go to Idle_B after a while
+        if (stateInfo.IsName("Base Layer.Idle_A"))
+        {
+            timer += Time.deltaTime;
+            if(timer > waitToTurnHead)
+            {
+                Debug.Log("timer:" + timer);
+                animator.SetBool("turnidle", true);
+                //timer = 0;
+            }
+        }
+
+        //if current state is Idle_B, go back to Idle_A when animation is done
+        if (stateInfo.IsName("Base Layer.Idle_B"))
+        {
+            Debug.Log("turning head");
+            animator.SetBool("turnidle", false);
+            timer = 0;
+        }
+
+    }
+}
--- Assets/Scripts/testCharacterController.cs
+++ Assets/Scripts/testCharacterController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class testCharacterController : MonoBehaviour
 {
     public float speed = 6.0F;
+    public float turningSpeed = 3f;
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
 
@@ -18,6 +19,7 @@ public class testCharacterController : MonoBehaviour
 
     void FixedUpdate()
     {
+        // if controller is grounded: get move direction and speed
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -26,6 +28,15 @@ public class testCharacterController : MonoBehaviour
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
         }
+
+        //issues with going backwards
+        if(moveDirection != Vector3.zero)
+        {
+            var step = turningSpeed * Time.deltaTime;
+            Quaternion newRotation = Quaternion.LookRotation(moveDirection, transform.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, step);
+        }
+
 
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
