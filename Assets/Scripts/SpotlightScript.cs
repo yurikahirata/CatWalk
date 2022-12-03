d8c3904aa563ee5d257c1a2037691957ccb7343a
yurikahirata <yurikahirata0130@gmail.com>
2022-12-03 20:25:11 +0100

    Spotlight script

---
+++ Assets/Scripts/SpotlightScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightScript : MonoBehaviour
+{
+
+    public bool swingFull;
+    public bool swingHalf;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (swingFull)
+        {
+
+        }
+    }
+}
