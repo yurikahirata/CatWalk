967fdd63c1836eeaa90caf6b15033107117d0e4d
yurikahirata <yurikahirata0130@gmail.com>
2022-12-06 00:26:02 +0100

    Moving spotlight methods

--- Assets/Scripts/SpotlightScript.cs
+++ Assets/Scripts/SpotlightScript.cs
@@ -7,17 +7,43 @@ public class SpotlightScript : MonoBehaviour
 
     public bool swingFull;
     public bool swingHalf;
+    public float speed;
+    private float swingMax;
+    private float swingMin;
+    private float direction;
 
     void Start()
     {
-        
+        swingMax = 0.9f;
+        swingMin = 0.5f;
+        direction = 1;
     }
 
     void Update()
     {
-        if (swingFull)
+        if (swingFull || swingHalf)
         {
+            if (swingHalf)
+            {
+                swingMax = 0.9f;
+                swingMin = 0.75f;
+            } else
+            {
+                swingMax = 0.9f;
+                swingMin = 0.5f;
+            }
 
+            if (transform.rotation.x > swingMax)
+            {
+                direction = -1;
+                Debug.Log("Changed directions");
+            }
+
+
+            if (transform.rotation.x < swingMin)
+                direction = 1;
+
+            transform.Rotate(Time.deltaTime * speed * direction, 0, 0);
         }
     }
 }
