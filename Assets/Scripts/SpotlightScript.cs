b3c27d67e20430b8b95025af4001ea2627db8d60
yurikahirata <yurikahirata0130@gmail.com>
2022-12-09 11:57:13 +0100

    rotate level

--- Assets/Scripts/SpotlightScript.cs
+++ Assets/Scripts/SpotlightScript.cs
@@ -10,7 +10,8 @@ public class SpotlightScript : MonoBehaviour
     public float speed;
     private float swingMax; // maximum angle
     private float swingMin; // minimum angle
-    private float direction; // forward or background
+    private float direction; // forward or backwards
+  
 
     void Start()
     {
@@ -31,16 +32,17 @@ public class SpotlightScript : MonoBehaviour
                 swingMin = 0.5f; // approx. 60 degrees
             }
 
-            if (transform.rotation.x > swingMax || transform.rotation.z > swingMax)
+            if (transform.rotation.x > swingMax)
             {
                 direction = -1;
             }
 
 
-            if (transform.rotation.x < swingMin || transform.rotation.z < swingMin)
+            if (transform.rotation.x < swingMin)
                 direction = 1;
 
             transform.Rotate(Time.deltaTime * speed * direction, 0, 0);
+
         }
     }
 }
