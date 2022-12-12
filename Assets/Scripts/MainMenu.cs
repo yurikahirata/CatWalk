3099b6c90f8f9981bb64c8a257a687e74a9c9783
yurikahirata <yurikahirata0130@gmail.com>
2022-12-12 15:09:16 +0100

    clean up + level select

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,4 +11,39 @@ public class MainMenu : MonoBehaviour
 
     }
 
+    public void SelectLevels()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+
+    public void Level6()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+
 }
