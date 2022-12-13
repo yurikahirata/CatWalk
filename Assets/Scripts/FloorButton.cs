03d94d33ac2757240c4993af53115f87dfb33914
yurikahirata <yurikahirata0130@gmail.com>
2022-12-13 14:55:34 +0100

    robot prefab, level 4 floor button

--- Assets/Scripts/FloorButton.cs
+++ Assets/Scripts/FloorButton.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class FloorButton : MonoBehaviour
 {
     public GameObject platform;
+    public bool ifLevel4;
 
     // Start is called before the first frame update
     void Start()
     {
         platform.SetActive(false);
+        if (ifLevel4)
+            gameObject.GetComponent<Renderer>().material.color = new Color(0.3f,0.3f,0.3f);
 
     }
 
@@ -33,7 +36,10 @@ public class FloorButton : MonoBehaviour
         if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Trigger"))
         {
             platform.SetActive(false);
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            if (ifLevel4)
+                gameObject.GetComponent<Renderer>().material.color = new Color(0.3f, 0.3f, 0.3f);
+            else
+                gameObject.GetComponent<Renderer>().material.color = Color.red;
         }
     }
 }
